0ea8e187d2b48df20c4b5f96d8842d105dd6f4d8
Carlos <carlos@Alonsos-iMac.local>
2020-02-11 17:00:16 -0600

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="Header">
+    <div class="Header__image">
+      <img alt="logo" src="https://orderingweb.ordering.co/templates/web/img/logo.png" />
+    </div>
+    <div class="Header__right_menu">
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Login</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.Header {
+  height: 40px;
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  border-radius: 10px;
+}
+
+.Header__image img {
+  height: 100%;
+}
+
+.Header__right_menu {
+  width: 300px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
